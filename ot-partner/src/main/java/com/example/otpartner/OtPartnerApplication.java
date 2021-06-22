0008d3a95a2c676ad9e3d9703b7992cbc3b9ab33
dergecz.peter <peterdergecz@gmail.com>
2021-06-22 10:52:28 +0200

    alapok

---
+++ ot-partner/src/main/java/com/example/otpartner/OtPartnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.otpartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtPartnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtPartnerApplication.class, args);
+    }
+
+}
---
+++ ot-partner/src/test/java/com/example/otpartner/OtPartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otpartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtPartnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
